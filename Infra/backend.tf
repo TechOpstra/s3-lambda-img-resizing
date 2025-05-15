4b790d9a9fbc74006de4e2d35a26b6f809ebe840
Harish <107391575+8555936327@users.noreply.github.com>
2025-05-15 10:13:58 +0530

    Update backend.tf

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     bucket       = "techopsbucket123"
-    key          = "vpcec2/terraform.tfstateimageresize"
+    key          = "vpcec2/terraform.tfstateimageresize1"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
